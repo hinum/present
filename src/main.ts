e7f6b0ed38be128862a335e2b23059f0c99fe249
hinum <sb5719@sb-school.ac.th>
2024-07-23 08:39:00 +0700

    djwuwuw

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import kaboom, { Vec2 } from "kaplay"
 import { Color } from "kaplay/dist/declaration/math"
 import { calculateConst, promisify, smoothMove } from "./utils"
+import { flavorEntries, flavors } from "@catppuccin/palette"
 
 const WIDTH = 100
 const ASPECT_RATIO = (16 / 9)
@@ -11,6 +12,11 @@ export const k = kaboom({
   height: WIDTH * (1 / ASPECT_RATIO),
   scale: innerWidth / WIDTH,
 })
+
+const latte = Object.fromEntries(flavors.latte.colorEntries.map(([name, {rgb:{r,g,b}}])=>[name, k.rgb(r,g,b)]))
+const mocha = Object.fromEntries(flavors.mocha.colorEntries.map(([name, {rgb:{r,g,b}}])=>[name, k.rgb(r,g,b)]))
+
+k.setBackground(latte.base)
 
 const damp = 2
 const speed = 0.5
@@ -61,7 +67,7 @@ const makeBubbleColumn = (
     k.lifespan(110 / particle.speed),
     k.color(particle.color),
     k.circle(particle.radius * k.rand(1, 1.2)),
-    k.opacity(1),
+    k.opacity(0.75),
     k.pos(0,0),
 
     {rand: Math.random()}
@@ -79,7 +85,7 @@ const cloudOpts = {
   radius: 4,
   spawnInterval: 0.4,
   variationY: 5,
-  color: k.rgb(0,0,0)
+  color: latte.overlay2
 }
 const cloudLeft = makeClouds( k.vec2(20, 40), 20, cloudOpts )
 const cloudRight= makeClouds( k.vec2(80, 10), 20, cloudOpts )
@@ -88,7 +94,7 @@ const bubbleOpts = {
   spawnInterval: 1.25,
   radius: 3,
   speed: 10,
-  color: k.BLACK
+  color: latte.lavender
 }
 const bubbleLeft = makeBubbleColumn( k.vec2(20, 70), bubbleOpts )
 const bubbleRight= makeBubbleColumn( k.vec2(80, 90), bubbleOpts )
@@ -98,14 +104,21 @@ const name = k.add([
   k.anchor("center"),
   k.pos(k.center()),
   k.opacity(1),
-  k.color(k.BLACK)
+  k.color(latte.text)
 ])
 const nameTopText = k.add([
   k.text("presenting..", { size: 3, font}),
   k.anchor("bot"),
   k.pos(k.center().add(0, 7)),
   k.opacity(1),
-  k.color(k.BLACK)
+  k.color(latte.subtext1)
+])
+const sun = k.add([
+  k.circle(5),
+  k.z(-10),
+  k.color(latte.surface2),
+  k.anchor("center"),
+  smoothMove(15,10, damp, speed)
 ])
 
 name.fadeIn(1)
@@ -119,18 +132,13 @@ async function main(){
   cloudLeft.smoothBy( -60, 0 )
   cloudRight.smoothBy( 60, 0 )
 
-  await k.wait(1.5)
+  await k.wait(1)
 
   name.fadeOut(1)
   k.wait(0.2, ()=>nameTopText.fadeOut(1))
   bubbleLeft.smoothBy( -40, 0)
   bubbleRight.smoothBy( 40, 0)
 
-  const sea = k.add([
-    k.rect(100, 100),
-    k.color(k.BLACK),
-    smoothMove(0, k.height(), damp, speed)
-  ])
   const boat = k.add([
     k.rect(10,10),
     k.color(k.BLACK),
@@ -138,6 +146,21 @@ async function main(){
     k.anchor("center"),
     smoothMove(60, k.height() + 5, 0.4, speed)
   ])
+  const sea = k.add([
+    k.rect(100, 100),
+    k.color(latte.lavender),
+    smoothMove(0, k.height(), damp, speed)
+  ])
+  cloudLeft.teleportTo( -20,10)
+  cloudRight.teleportTo( 120,30)
+
+  await k.wait(1)
+  sun.smoothTo( 80, 10)
+  sea.smoothBy( 0, -10 )
+  cloudLeft.smoothBy( 40, 0)
+  cloudRight.smoothBy(-40, 0)
+  
+  await k.wait(0.5)
   const paragraph1 = `
 uhhhhhhhhgh
 uhhhhuyh
@@ -146,23 +169,19 @@ yeah :thumbsup:
   const paragraph = k.add([
     k.text(paragraph1, { size: 3, font}),
     k.anchor("botleft"),
-    k.pos(5, k.height()-15),
+    smoothMove(5, k.height()-10, damp, speed),
     k.opacity(1),
     k.color(k.BLACK)
   ])
+  paragraph.smoothBy(0,-5)
+  paragraph.fadeIn(1)
 
-  cloudLeft.teleportTo( -20,10)
-  cloudRight.teleportTo( 120,30)
-
-  await k.wait(1)
-  sea.smoothBy( 0, -10 )
-  cloudLeft.smoothBy( 40, 0)
-  cloudRight.smoothBy(-40, 0)
 
   await k.wait(1.5)
   boat.smoothBy(0,-16)
 
   await awaitNext()
+  paragraph.fadeOut()
   const boatLastPos = boat.pos.clone()
   
   k.tween(0,1, 2, t=>{
@@ -174,9 +193,15 @@ yeah :thumbsup:
   await k.wait(1.5)
   sea.constants = calculateConst(1.7, 0.2)
   sea.smoothBy(0,-70)
+  sun.smoothBy(0,-40)
   cloudLeft.smoothBy(-40, 0)
   cloudRight.smoothBy(40, 0)
 
+  await k.tween(0,1,2, t=>{
+    sea.color.r = mocha.surface0.r * t
+    sea.color.g = mocha.surface0.g * t
+    sea.color.b = mocha.surface0.b * t
+  })
 }
 main()
 
